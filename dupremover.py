e1f078d3b8f48a6b6d40ca919fadb0190ce66531
Heeone Lee <heeone@DESKTOP-DMMLUKA.localdomain>
2021-03-30 16:41:02 +0900

    fixing error on dup_index.

--- dupremover.py
+++ dupremover.py
@@ -1,56 +1,55 @@
-try:
-    file_1 = input('병합할 첫번째 파일명을 확장명과 함께 입력해 주세요')
-    file_2 = input('병합할 두번째 파일명을 확장명과 함께 입력해 주세요')
-except:
-    print('일치하는 파일이 없습니다.')
+file_1 = input('병합할 첫번째 파일명을 확장명과 함께 입력해 주세요 ')
+file_2 = input('병합할 두번째 파일명을 확장명과 함께 입력해 주세요 ')
 
 distinctlines1 = []
 count1 = 0
 dup_index = []
-with open(file_1, 'r') as f1:
-    for i in f1.readlines():
+lines1 = []
+lines2 = []
+
+#같은 문장이 있으면 모두 삭제
+with open(file_1, 'r') as f1:    
+    for i in f1.read().split('\n'):
+        lines1.append(i)
         if i not in distinctlines1:
             distinctlines1.append(i)
         elif i in distinctlines1:
             dup_index.append(count1)
+            dup_index.append(lines1.index(i))   
         count1 += 1
 
 distinctlines2 = []
 count2 = 0
+
 with open(file_2, 'r') as f2:
-    for i in f2.readlines():
+    for i in f2.read().split('\n'):
+        lines2.append(i)
         if i not in distinctlines2:
             distinctlines2.append(i)
         elif i in distinctlines2 and count2 not in dup_index:
             dup_index.append(count2)    
-        count2 += 1
+            dup_index.append(lines2.index(i))   
+        count2 += 1      
 
-lines1 = []
-lines2 = []
-joined = []
-with open(file_1, 'r') as f1:
-    for i in f1.readlines():
-        lines1.append(i)
-with open(file_2) as f2:
-    for i in f2.readlines():
-        lines2.append(i)
-for i in range(len(lines1)):
-    joined.append((lines1[i]+'\t'+lines2[i]))
+print(set(dup_index).reverse())
+"""
+for i in set(dup_index.sort(reverse=True)):
+    print(lines1[i])
+    print(lines2[i])
 
-for i in dup_index:
-    del joined[i]
-        
+
 distinctfile1 = []
 distinctfile2 = []
 for line in joined:
     distinctfile1.append(line.split('\t')[-1])
     distinctfile2.append(line.split('\t')[0])
 
-fname1 = open(input('첫번째 언어쌍 파일을 저장할 파일명을 지정해 주세요(확장명까지 입력해 주세요)'),'w')
-fname2 = open(input('두번째 언어쌍 파일을 저장할 파일명을 지정해 주세요(확장명까지 입력해 주세요)'),'w')
+fname1 = open(input('첫번째 언어쌍 파일을 저장할 파일명을 지정해 주세요(확장명까지 입력해 주세요) '),'w')
+fname2 = open(input('두번째 언어쌍 파일을 저장할 파일명을 지정해 주세요(확장명까지 입력해 주세요) '),'w')
 
 fname1.write(str('\n'.join(distinctfile1)))
 fname1.close()        
 
 fname2.write(str('\n'.join(distinctfile2)))
 fname2.close()  
+"""
