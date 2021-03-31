8c50f10be980274083246f0bd3b8625b0c69023a
Heeone Lee <heeone@DESKTOP-DMMLUKA.localdomain>
2021-03-31 11:07:12 +0900

    added user input questions

--- TSV_join.py
+++ TSV_join.py
@@ -1,4 +1,3 @@
-
 def join(file1, file2):
     lines1 = []
     lines2 = []
@@ -11,4 +10,12 @@ def join(file1, file2):
                 lines2.append(line2)
     for i in range(len(lines1)):
         joined.append((lines1[i]+'\t'+lines2[i]))
-    return str('\n'.join(joined))    
+
+    save = open(input("저장할 파일명을 확장명과 함께 입력해 주세요 "),'w')
+    save.write(str('\n'.join(joined)))
+    save.close()  
+
+file_1 = input("병합할 첫번째 파일 명을 확장명과 함께 입력해 주세요 ")
+file_2 = input("병합할 두번째 파일 명을 확장명과 함께 입력해 주세요 ")
+
+join(file_1, file_2)
--- TSV_separate.py
+++ TSV_separate.py
@@ -1,7 +1,17 @@
+line1 = []
+line2 = []
 def separate(file):
     with open(file) as f:
         for line in f.read().split('\n'):
-            print(line.split('\t')[-1])
+            line1.append(line.split('\t')[0])
+            line2.append(line.split('\t')[-1])
 
-filename = input('분리할 파일명을 확장명과 함께 입력해 주세요.')
-print(separate("zh_en.txt"))
+separate(input('분리할 파일명을 확장명과 함께 입력해 주세요 '))
+
+file1 = open(input('첫번째 열의 데이터를 저장할 파일명을 확장명과 함께 입력해 주세요 '), 'w')
+file2 = open(input('두번째 열의 데이터를 저장할 파일명을 확장명과 함께 입력해 주세요 '), 'w')
+
+file1.write(str('\n'.join(line1)))
+file2.write(str('\n'.join(line2)))
+file1.close()
+file2.close()
